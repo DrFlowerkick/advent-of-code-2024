0380572f100ff6f4224bfcba4836aec9cced031e
DrFlowerkick <flowerkick14@gmail.com>
2024-12-04 09:51:50 +0100

    Day 04 finished

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -41,24 +41,25 @@ struct Day03DataWithFlags {
 impl From<&str> for Day03DataWithFlags {
     fn from(value: &str) -> Self {
         // Regex for mul(XXX,YYY) or do() or don't()
-        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").expect("Failed to create regex");
+        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")
+            .expect("Failed to create regex");
         let mut factors: Vec<(i128, i128)> = Vec::new();
         let mut enabled = true;
         for cap in re.captures_iter(value) {
             match cap.get(0).unwrap().as_str() {
                 "do()" => enabled = true,
                 "don't()" => enabled = false,
-                _ => if enabled {
-                    let n1 = cap.get(1).unwrap().as_str().parse::<i128>().unwrap();
-                    let n2 = cap.get(2).unwrap().as_str().parse::<i128>().unwrap();
-                    factors.push((n1, n2));
-                },
+                _ => {
+                    if enabled {
+                        let n1 = cap.get(1).unwrap().as_str().parse::<i128>().unwrap();
+                        let n2 = cap.get(2).unwrap().as_str().parse::<i128>().unwrap();
+                        factors.push((n1, n2));
+                    }
+                }
             }
         }
-        
-        Self {
-            factors,
-        }
+
+        Self { factors }
     }
 }
 
@@ -71,16 +72,16 @@ impl Day03DataWithFlags {
 pub fn day_03() -> Result<()> {
     let input = include_str!("../../assets/day_03.txt");
     let challenge = Day03Data::from(input);
-    
+
     let result_part1 = challenge.add_up_multiplied_factors();
     println!("result day 03 part 1: {}", result_part1);
     assert_eq!(result_part1, 171_183_089);
-    
+
     let challenge = Day03DataWithFlags::from(input);
     let result_part2 = challenge.add_up_multiplied_factors();
     println!("result day 03 part 2: {}", result_part2);
     assert_eq!(result_part2, 63_866_497);
-    
+
     Ok(())
 }
 
@@ -93,17 +94,17 @@ mod tests {
     fn test_example_part() -> Result<()> {
         let input = include_str!("../../assets/day_03_example_01.txt");
         let challenge = Day03Data::from(input);
-        
+
         let result_part1 = challenge.add_up_multiplied_factors();
         println!("result day 03 part 1: {}", result_part1);
         assert_eq!(result_part1, 161);
-        
+
         let input = include_str!("../../assets/day_03_example_02.txt");
         let challenge = Day03DataWithFlags::from(input);
         let result_part2 = challenge.add_up_multiplied_factors();
         println!("result day 03 part 2: {}", result_part2);
         assert_eq!(result_part2, 48);
-        
+
         Ok(())
     }
 }
