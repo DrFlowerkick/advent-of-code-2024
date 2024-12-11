ebba47d8c242c4b85d343b5bfea4b14b543e858a
DrFlowerkick <flowerkick14@gmail.com>
2024-12-11 14:56:50 +0100

    Finished Day11

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -1,30 +1,86 @@
 //!day_11.rs
 
+use crate::utilities::digit_count;
 use anyhow::Result;
+use std::collections::HashMap;
 
 #[derive(Debug)]
-struct Day11Data {}
+struct Day11Data {
+    challenge: Vec<u128>,
+    blinks: u32,
+}
 
 impl From<&str> for Day11Data {
-    fn from(_value: &str) -> Self {
-        Self {}
+    fn from(value: &str) -> Self {
+        Self {
+            challenge: value
+                .split_whitespace()
+                .map(|n| n.parse::<u128>().unwrap())
+                .collect::<Vec<u128>>(),
+            blinks: 0,
+        }
     }
 }
 
-impl Day11Data {}
+impl Day11Data {
+    fn set_blinks(&mut self, blinks: u32) {
+        self.blinks = blinks;
+    }
+    fn calc_stones(&self) -> u128 {
+        let mut num_stones = 0;
+        let mut cache: HashMap<(u128, u32), u128> = HashMap::new();
+        for digit in self.challenge.iter() {
+            num_stones += calc_stones_recursive(*digit, self.blinks, &mut cache);
+        }
+        num_stones
+    }
+}
+
+fn calc_stones_recursive(
+    digit: u128,
+    num_blinks: u32,
+    cache: &mut HashMap<(u128, u32), u128>,
+) -> u128 {
+    if num_blinks == 0 {
+        return 1;
+    }
+    if let Some(stones) = cache.get(&(digit, num_blinks)) {
+        return *stones;
+    }
+    let stones = if digit == 0 {
+        calc_stones_recursive(1, num_blinks - 1, cache)
+    } else {
+        let n_digits = digit_count(digit);
+        if n_digits & 1 == 1 {
+            // odd number of digits
+            calc_stones_recursive(digit * 2024, num_blinks - 1, cache)
+        } else {
+            // even number of digits
+            let factor = 10_u128.pow(n_digits / 2);
+            let left = digit / factor;
+            let right = digit % factor;
+            calc_stones_recursive(left, num_blinks - 1, cache)
+                + calc_stones_recursive(right, num_blinks - 1, cache)
+        }
+    };
+    cache.insert((digit, num_blinks), stones);
+    stones
+}
 
 pub fn day_11() -> Result<()> {
     let input = include_str!("../../assets/day_11.txt");
-    let _challenge = Day11Data::from(input);
-    /*
-    let result_part1 = challenge
-    println!("result day 11 part 1: {}", result_part1);
-    assert_eq!(result_part1, XXX);
+    let mut challenge = Day11Data::from(input);
+    challenge.set_blinks(25);
 
-    let result_part2 = challenge
+    let result_part1 = challenge.calc_stones();
+    println!("result day 11 part 1: {}", result_part1);
+    assert_eq!(result_part1, 217_812);
+
+    challenge.set_blinks(75);
+    let result_part2 = challenge.calc_stones();
     println!("result day 11 part 2: {}", result_part2);
-    assert_eq!(result_part2, XXX);
-    */
+    assert_eq!(result_part2, 259_112_729_857_522);
+
     Ok(())
 }
 
@@ -36,17 +92,14 @@ mod tests {
     #[test]
     fn test_example_part() -> Result<()> {
         let input = include_str!("../../assets/day_11_example.txt");
-        let _challenge = Day11Data::from(input);
+        let mut challenge = Day11Data::from(input);
+        challenge.set_blinks(25);
 
-        /*
-        let result_part1 = challenge
+        let result_part1 = challenge.calc_stones();
         println!("result day 11 part 1: {}", result_part1);
-        assert_eq!(result_part1, XXX);
+        assert_eq!(result_part1, 55_312);
 
-        let result_part2 = challenge
-        println!("result day 11 part 2: {}", result_part2);
-        assert_eq!(result_part2, XXX);
-        */
+        // no test result for part 2
 
         Ok(())
     }
