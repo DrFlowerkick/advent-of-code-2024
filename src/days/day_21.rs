3e808ee9d1ff5126bd9713cadc6201aacfbede2a
DrFlowerkick <flowerkick14@gmail.com>
2024-12-21 21:53:01 +0100

    Finished Part 1. WOW!

--- src/days/day_21.rs
+++ src/days/day_21.rs
@@ -36,68 +36,17 @@ impl KeyPad {
         let from_pos = self.keys.get(&from).unwrap();
         let to_pos = self.keys.get(&to).unwrap();
         let delta_x = to_pos.0 - from_pos.0;
-        let dir_char = if delta_x > 0 { '>' } else { '<' };
-        let dir_x: Vec<char> = vec![dir_char; delta_x.abs() as usize];
+        let dir_char = if delta_x > 0 { ">" } else { "<" };
+        let dir_x = dir_char.to_string().repeat(delta_x.abs() as usize);
         let delta_y = to_pos.1 - from_pos.1;
         // y goes from top to bottom!
-        let dir_char = if delta_y < 0 { '^' } else { 'v' };
-        let dir_y: Vec<char> = vec![dir_char; delta_y.abs() as usize];
-        let mut sequence = String::new();
-        let mut index_x = 0;
-        let mut index_y = 0;
-        let mut current = from;
-        loop {
-            if index_x < dir_x.len() {
-                if let Some(new_from) = self.key_from_dir(current, dir_x[index_x]) {
-                    sequence.push(dir_x[index_x]);
-                    current = new_from;
-                    index_x += 1;
-                    continue;
-                }
-            }
-            if index_y < dir_y.len() {
-                if let Some(new_from) = self.key_from_dir(current, dir_y[index_y]) {
-                    sequence.push(dir_y[index_y]);
-                    current = new_from;
-                    index_y += 1;
-                    continue;
-                }
-            }
-            if index_x == dir_x.len() && index_y == dir_y.len() {
-                return sequence + "a";
-            }
-            panic!("Unexpected sequence error from '{}' to '{}' (dir_x: {:?}, dir_y:{:?}", from, to, dir_x, dir_y);
-        }
-        /*
-        if from.0 == 0 {
-            format!("{}{}a", dir_x, dir_y)
-        } else {
+        let dir_char = if delta_y < 0 { "^" } else { "v" };
+        let dir_y = dir_char.to_string().repeat(delta_y.abs() as usize);
+        if (from_pos.1 == 3 && to_pos.0 == 0) || (to == '<' && from_pos.1 == 0) {
             format!("{}{}a", dir_y, dir_x)
-        } */
-    }
-    fn key_from_dir(&self, from: char, dir: char) -> Option<char> {
-        let dir = match dir {
-            '^' => (0, -1),
-            '<' => (-1, 0),
-            'v' => (0, 1),
-            '>' => (1, 0),
-            _ => panic!("unexpected dir."),
-        };
-        let num_pad = "0123456789A";
-        let dir_pad = "^<v>a";
-        let is_num_pad = num_pad.contains(from);
-        if let Some(pos) = self.keys.get(&from) {
-            let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
-            return self
-                .keys
-                .iter()
-                .find(|(k, v)| {
-                    **v == new_pos && ((is_num_pad && num_pad.contains(**k))
-                        || (!is_num_pad && dir_pad.contains(**k)))
-                })
-                .map(|(k, _)| *k);
+        } else {
+            format!("{}{}a", dir_x, dir_y)
         }
-        None
     }
 }
 
@@ -130,6 +79,7 @@ impl Day21Data {
             let sequence = self.get_num_pad_sequence(&keypad, &code);
             dbg!(code);
             dbg!(&sequence);
+            dbg!(sequence.len());
             let num_value: usize = code[..3].parse::<usize>().unwrap();
             complexities += sequence.len() * num_value;
             dbg!(sequence.len() * num_value);
@@ -138,55 +88,55 @@ impl Day21Data {
     }
     fn get_num_pad_sequence(&mut self, keypad: &KeyPad, code: &str) -> String {
         let mut sequence = String::new();
-        let mut previous_char = 'A';
+        let mut previous_key = 'A';
         for key in code.chars() {
-            if let Some(cached_sequence) = self.cache_num_pad.get(&(previous_char, key)) {
+            if let Some(cached_sequence) = self.cache_num_pad.get(&(previous_key, key)) {
                 //println!("rc bot: {}", cached_sequence);
                 sequence += cached_sequence;
-                previous_char = key;
+                previous_key = key;
                 continue;
             }
-            let radiation_bot_sequence = self.get_key_strokes(keypad, previous_char, key);
+            let radiation_bot_sequence = self.get_key_strokes(keypad, previous_key, key);
             //println!("r  bot: {}", radiation_bot_sequence);
             let key_sequence = self.get_radiation_dir_pad_sequence(keypad, &radiation_bot_sequence);
             sequence += &key_sequence;
-            if (previous_char, key) == ('3', '7') {
+            if (previous_key, key) == ('3', '7') {
                 println!("('3', '7'): {}", key_sequence);
             }
             self.cache_num_pad
-                .insert((previous_char, key), key_sequence);
-            previous_char = key;
+                .insert((previous_key, key), key_sequence);
+            previous_key = key;
         }
         sequence
     }
     fn get_radiation_dir_pad_sequence(&mut self, keypad: &KeyPad, code: &str) -> String {
         let mut sequence = String::new();
-        let mut previous_char = 'a';
+        let mut previous_key = 'a';
         for key in code.chars() {
-            if let Some(cached_sequence) = self.cache_radiation_dir_pad.get(&(previous_char, key)) {
+            if let Some(cached_sequence) = self.cache_radiation_dir_pad.get(&(previous_key, key)) {
                 //println!("fc bot: {}", cached_sequence);
                 sequence += cached_sequence;
-                previous_char = key;
+                previous_key = key;
                 continue;
             }
-            let frozen_bot_sequence = self.get_key_strokes(keypad, previous_char, key);
+            let frozen_bot_sequence = self.get_key_strokes(keypad, previous_key, key);
             //println!("f  bot: {}", frozen_bot_sequence);
             let key_sequence = self.get_frozen_dir_pad_sequence(keypad, &frozen_bot_sequence);
             sequence += &key_sequence;
             self.cache_radiation_dir_pad
-                .insert((previous_char, key), key_sequence);
-            previous_char = key;
+                .insert((previous_key, key), key_sequence);
+            previous_key = key;
         }
         sequence
     }
     fn get_frozen_dir_pad_sequence(&mut self, keypad: &KeyPad, code: &str) -> String {
         let mut sequence = String::new();
-        let mut previous_char = 'a';
+        let mut previous_key = 'a';
         for key in code.chars() {
-            let key_sequence = self.get_key_strokes(keypad, previous_char, key);
+            let key_sequence = self.get_key_strokes(keypad, previous_key, key);
             //println!("me    : {}", key_sequence);
             sequence += &key_sequence;
-            previous_char = key;
+            previous_key = key;
         }
         sequence
     }
@@ -204,12 +154,12 @@ impl Day21Data {
 
 pub fn day_21() -> Result<()> {
     let input = include_str!("../../assets/day_21.txt");
-    let _challenge = Day21Data::from(input);
-    /*
-    let result_part1 = challenge
+    let mut challenge = Day21Data::from(input);
+    
+    let result_part1 = challenge.calc_complexities();
     println!("result day 21 part 1: {}", result_part1);
-    assert_eq!(result_part1, XXX);
-
+    assert_eq!(result_part1, 197_560);
+    /*
     let result_part2 = challenge
     println!("result day 21 part 2: {}", result_part2);
     assert_eq!(result_part2, XXX);
@@ -221,14 +171,6 @@ pub fn day_21() -> Result<()> {
 mod tests {
 
     use super::*;
-
-    #[test]
-    fn test_key_from_dir() {
-        let key_pad = KeyPad::default();
-        assert_eq!(key_pad.key_from_dir('0', '<'), None);
-        assert_eq!(key_pad.key_from_dir('^', '<'), None);
-
-    }
 
     #[test]
     fn test_key_strokes() {
